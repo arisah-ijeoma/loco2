67b2a469438ff79e07c5beedd7ba413ae50f6723
Job Candidate <anonymous@loco2.com>
2019-03-21 00:53:55 +0100

    Add pseudo code for testing

---
+++ spec/scripts/search_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# SEARCH
+  # number of search results in sample xml
+  # number of train changes (connections)
+  # fare names and prices; compare fares in different connections (one connection
+  # might have first class and another would not)
+  # add value of connection, determine cheapest in sample data
+  # check time for a full trip, determine fastest in sample data
+# TIME
+  # arrival and departure time are in local time
+    # use the date to determine what the time should be calculated
+  # duration of connection in hours and minutes
+  # duration before connection in trips
+
+describe 'search information' do
+
+end
