567fb9dd47631c2a0facb9d6fcda0edc3ab45992
Job Candidate <anonymous@loco2.com>
2019-03-22 12:26:53 +0100

    refactor specs to accommodate fare spec

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -4,9 +4,8 @@ require 'rails_helper'
 
 # SEARCH
   # *** number of search results in sample xml
-  # number of train changes (connections)
-  # fare names and prices; compare fares in different connections (one connection
-  # might have first class and another would not)
+  # *** number of train changes (connections)
+  # fare names and prices; compare fares in different connections (one connection might have first class and another would not)
   # add value of connection, determine cheapest in sample data
   # check time for a full trip, determine fastest in sample data
 # TIME
@@ -24,13 +23,39 @@ describe 'search information' do
       expect(search.results.count).to eq(3)
     end
 
-    describe 'connections' do
-      let(:first_trip) { search.results.first['ID'] }
-      let(:last_trip) { search.results.last['ID'] }
+    describe 'trips' do
+      let(:connections) { search.connections[trip] }
 
-      it 'has the correct number of connections per trip' do
-        expect(search.connections[first_trip]).to eq(2)
-        expect(search.connections[last_trip]).to eq(3)
+      context 'trip 1' do
+        let(:trip) { search.results.first['ID'] }
+
+        describe 'connections' do
+          it 'has the correct number of connections' do
+            expect(connections).to eq(2)
+          end
+        end
+
+        describe 'fare' do
+          it 'returns the correct fare for trip' do
+
+          end
+        end
+      end
+
+      context 'trip 2' do
+        let(:trip) { search.results.last['ID'] }
+
+        describe 'connections' do
+          it 'has the correct number of connections' do
+            expect(connections).to eq(3)
+          end
+        end
+
+        describe 'fare' do
+          it 'returns the correct fare for trip' do
+
+          end
+        end
       end
     end
   end
