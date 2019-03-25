451358415ee1ec480e5bab0920aeb896c3952a0f
Job Candidate <anonymous@loco2.com>
2019-03-25 16:32:35 +0100

    modify failing spec

--- spec/models/script_spec.rb
+++ spec/models/script_spec.rb
@@ -4,13 +4,20 @@ require 'rails_helper'
 
 describe 'script' do
   describe '.user_input' do
-    it 'returns only valid user input' do
-      expect(Script.user_input).to eq('0').or(eq('1')).or(eq('2'))
+    describe do
+      it 'returns only valid user input' do
+        allow(STDIN).to receive(:gets) { '1' }
+        expect(Script.user_input).to eq('1')
+      end
     end
 
-    it 'does not return invalid input' do
-      expect(Script.user_input).not_to eq('3')
-      expect(Script.user_input).not_to eq('w')
+    describe 'invalid input' do
+      it 'does not return invalid input' do
+        allow(STDIN).to receive(:gets) { '3' }
+        allow(STDIN).to receive(:gets) { '2' }
+        expect(Script.user_input).not_to eq('3')
+        expect(Script.user_input).to eq('2')
+      end
     end
   end
 end
